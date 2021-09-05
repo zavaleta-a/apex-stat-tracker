f6eae18aae9c082e5bbf3676aaab59dca502c3f7
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-05 05:43:01 -0500

    Reviewing nav bar on tablet, was styled

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,13 +2,14 @@ $(function () {
   var mq = matchMedia("(max-width: 1080px)");
   if (mq.matches) {
     $("figure").css("display", "inline-block");
-    var insertBreak = /*$("<div></div>").append*/ $("<br />");
-    $("#layout-Tablet").append(insertBreak);
     $("#logo")
       .removeClass("brand-logo center")
       .css({ position: "relative", left: "33.5%", fontSize: "2rem" });
+    $("#ul-Background").css("background-color", "green");
     $("ul").addClass("col m2").css({ position: "relative", left: "42%" });
     $("li").css("margin-bottom", "-10px");
     $("hr").remove();
+  } else {
+    $("ul").css({ display: "flex", position: "relative", left: "82%" });
   }
 });
