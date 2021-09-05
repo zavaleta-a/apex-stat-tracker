5688ea1873d887a6e1201b3e11fe94c46c698816
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-05 00:49:00 -0500

    Debugging jQuery code for responsiveness

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,15 @@
-$(function () {});
+$(function () {
+  var mq = matchMedia("(max-width: 1200px)");
+  if (mq.matches) {
+    console.log("TEST RETURNS TRUE");
+  } else {
+    console.log("TEST RETURNS FALSE");
+  }
+});
+
+/*matchMedia("(max-width: 1200px)");
+if (matchMedia.matches) {
+  console.log("True");
+} else {
+  console.log("False");
+}*/
