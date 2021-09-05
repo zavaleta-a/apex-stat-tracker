fd2b476f417a358d25964c47711d1e893e912267
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-05 02:01:48 -0500

    Further improved responsiveness for tablet layout, added nav bar to bottom of logo

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,15 +1,8 @@
 $(function () {
   var mq = matchMedia("(max-width: 1200px)");
   if (mq.matches) {
-    console.log("TEST RETURNS TRUE");
-  } else {
-    console.log("TEST RETURNS FALSE");
+    $("figure").css("display", "block");
+    var insertBreak = $("<div></div>").append("<br />");
+    $("#layout-Tablet").append(insertBreak);
   }
 });
-
-/*matchMedia("(max-width: 1200px)");
-if (matchMedia.matches) {
-  console.log("True");
-} else {
-  console.log("False");
-}*/
