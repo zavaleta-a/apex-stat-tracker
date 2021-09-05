164e6738953a03c666cbfbadcecf8a264147002f
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-05 02:59:03 -0500

    testing nav bar responsiveness

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -2,8 +2,12 @@ $(function () {
   var mq = matchMedia("(max-width: 1200px)");
   if (mq.matches) {
     $("figure").css("display", "block");
-    var insertBreak = $("<div></div>").append("<br />");
+    var insertBreak = /*$("<div></div>").append*/ $("<br />");
     $("#layout-Tablet").append(insertBreak);
+    $("#logo")
+      .removeClass("brand-logo center")
+      .css({ position: "relative", left: "265px", fontSize: "2rem" });
     $("ul").css({ position: "relative", left: "32%" });
+    $("hr").remove();
   }
 });
