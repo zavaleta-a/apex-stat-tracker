ab36d48174ac0d9f6502981d89a7c388d369db47
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-05 04:19:17 -0500

    Changed nav layout on tablet, **testing**

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -7,7 +7,7 @@ $(function () {
     $("#logo")
       .removeClass("brand-logo center")
       .css({ position: "relative", left: "33%", fontSize: "2rem" });
-    $("ul").addClass("col m2").css({ position: "relative", left: "32%" });
+    $("ul").addClass("col m3").css({ position: "relative", left: "32%" });
     $("#about-Li").css({ position: "relative", right: "15px" });
     $("hr").remove();
   }
