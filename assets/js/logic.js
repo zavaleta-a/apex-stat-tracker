1871ec0a8efb31abff22b0734337c34049f910a4
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-07 15:01:18 -0500

    Restarting project starting with a mobile first approach

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,11 +1,1 @@
-$(function () {
-  var mq = matchMedia("(max-width: 1200px)");
-  if (mq.matches) {
-    $("#logo").removeClass("brand-logo center");
-    $("ul").addClass("col md3");
-    $("hr").remove();
-  } else {
-    $("ul").css({ display: "flex", position: "relative", left: "73%" });
-    $("ul").addClass("col l3.5");
-  }
-});
+$(function () {});
