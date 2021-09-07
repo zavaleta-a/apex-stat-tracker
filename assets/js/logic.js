674d6efad2dfcedcdcf33b70f0828cc2c861acb2
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-07 16:35:41 -0500

    Added logo to center when viewing on desktop screen or bigger

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,1 +1,7 @@
-$(function () {});
+$(function () {
+  var mq = matchMedia("(min-width: 992px)");
+  if (mq.matches) {
+    $(".flex-Container").addClass("brand-logo center");
+    $("#brand-Logo").css("width", "23%");
+  }
+});
