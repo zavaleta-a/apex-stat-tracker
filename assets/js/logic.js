b66bb96df0e03da877fee19ae513c81a96f9ca7e
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-08 17:11:47 -0500

    Added comment to Javascript file

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,4 +1,5 @@
 $(function () {
+  // If screen reaches width of 992px or higher, add a class to center logo and adjust width //
   var mq = matchMedia("(min-width: 992px)");
   if (mq.matches) {
     $(".flex-Container").addClass("brand-logo center");
