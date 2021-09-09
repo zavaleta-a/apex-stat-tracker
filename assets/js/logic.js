76602a26a112a30e8ba1b398997190488b069a1d
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-08 20:39:13 -0500

    Added mobile responsive design to search UI

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -1,8 +1,13 @@
 $(function () {
-  // If screen reaches width of 992px or higher, add a class to center logo and adjust width //
+  // If screen reaches large desktop or higher, add a class to center logo and adjust width //
   var mq = matchMedia("(min-width: 992px)");
   if (mq.matches) {
     $(".flex-Container").addClass("brand-logo center");
     $("#brand-Logo").css("width", "23%");
+  }
+
+  var mq1 = matchMedia("(max-width: 600px)");
+  if (mq1.matches) {
+    
   }
 });
