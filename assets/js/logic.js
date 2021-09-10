d3bec2fbe5cdeae7eab77fc7f9a5d09460802c72
Tino Carrier <tino.tyreecarrier@yahoo.com>
2021-09-09 22:01:30 -0500

    Added player search functionality to playstation database section

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -29,7 +29,9 @@ $(function () {
       var playerNameInput = $("<input type='username' />");
       playerSearchBox.append(playerNameLabel);
       playerSearchBox.append(playerNameInput);
-      var playerSearchButton = $("<button>search</button>");
+      var playerSearchButton = $(
+        "<button id='checkPlayerData'>search</button>"
+      );
       playerSearchBox.append(insertBreak);
       playerSearchBox.append(playerSearchButton);
     } else if ($("#XBOX").is(":checked")) {
@@ -38,33 +40,50 @@ $(function () {
         .html("Xbox Database")
         .addClass("center-align");
       playerSearchBox.append(xboxSearch);
-    } else {
+    } else if ($("#PC").is(":checked")) {
       playerSearchBox.empty();
       var pcSearch = $("<h6></h6>")
         .html("PC Database")
         .addClass("center-align");
       playerSearchBox.append(pcSearch);
+    } else {
+      // WHAT HAPPENS IF NO CONSOLE TYPE IS SELECTED????? //
     }
 
-    playerSearchButton.on("click", function (event) {
+    $("#checkPlayerData").on("click", function (event) {
       event.preventDefault();
 
-      fetch("https://apex-legends.p.rapidapi.com/stats/imshleepdawg/PS4", {
-        method: "GET",
-        headers: {
-          "x-rapidapi-host": "apex-legends.p.rapidapi.com",
-          "x-rapidapi-key":
-            "6669d5edcfmsh0a1b6b64e531adap1a09d1jsn0b4c7917e6bc",
-        },
-      })
-        .then(function (resp) {
-          return resp.json();
-        })
-        .then(function (data) {
-          console.log(data);
-        });
+      var url = "https://apex-legends.p.rapidapi.com/stats/imshleepdawg/PC";
 
-      // END OF FETCH//
+      if (playerNameInput.val() !== "") {
+        url = url.slice(54);
+        url =
+          "https://apex-legends.p.rapidapi.com/stats/" +
+          playerNameInput.val() +
+          "/PC";
+        fetch(url, {
+          method: "GET",
+          headers: {
+            "x-rapidapi-host": "apex-legends.p.rapidapi.com",
+            "x-rapidapi-key":
+              "6669d5edcfmsh0a1b6b64e531adap1a09d1jsn0b4c7917e6bc",
+          },
+        })
+          .then(function (resp) {
+            return resp.json();
+          })
+          .then(function (data) {
+            console.log(data);
+          });
+        // END OF FETCH //
+      } else {
+        playerNameInput.css({
+          borderStyle: "solid",
+          borderWidth: "2px",
+          borderColor: "red",
+        });
+      }
+      // End Of Else //
     });
   });
 });
